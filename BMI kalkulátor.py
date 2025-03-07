0e0d78a04a01bfaab73853c6ea1b44a2b159f6e0
franců <francupatrik@seznam.cz>
2025-03-07 22:36:00 +0100

    Základní kód pro výpočet bez GUI

---
+++ BMI kalkulátor.py
@@ -0,0 +1,29 @@
+'''Podváha: BMI nižší než 18,5
+Normální váha: BMI mezi 18,5 a 24,9
+Nadváha: BMI mezi 25 a 29,9
+Obezita: BMI 30 a více'''
+
+vaha = float(input("Vlož svojí váhu v KG "))
+vyska = float(input("Vlož svojí výšku v M ")) # BMI = hmotnost (kg) / (výška v metrech)².
+
+
+#BMI = vaha / (vyska)**2
+
+vyskaBMI = vyska**2
+BMI = vaha/vyskaBMI
+
+if BMI < 18.5:
+    print(f"Vaše BMI je {BMI:.3f}, dle BMI trpíte podváhou.")
+
+elif  BMI > 29:
+    print(f"Vaše BMI je {BMI:.3f}, dle BMI trpíte obezitou.")
+
+elif 25 <= BMI <= 29.9:
+    print(f"Vaše BMI je {BMI:.3f}, dle BMI trpíte nadváhou.")
+
+else:
+    print(f"Vaše BMI je {BMI:.3f}, dle BMI máte normální váhu.")
+
+
+
+#elif 18.5 <= BMI <= 24.9:
