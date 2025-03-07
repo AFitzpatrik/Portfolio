cf02ab053f6378725313131eb4e6eee251b1f453
franců <francupatrik@seznam.cz>
2025-03-07 23:19:27 +0100

    Začátek tvorby GUI

---
+++ GUI.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+from tkinter import messagebox
+from BMI_kalkulator_def inport vypocitej_bmi, ziskani_kategorie
+
+def spustit_vypocet():
+    try:
+        #Načtení hodnot z vstupních polí
+        vaha = float(entry_vaha.get())
+        vyska = float(entry_vyska.get())
+
+
+
+# https://chat.deepseek.com/a/chat/s/b06aa235-b74a-4f7e-aadb-4d20dc1aac34
